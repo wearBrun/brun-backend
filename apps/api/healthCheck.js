7d30626ad7025d115e93c587eb03f9a903840ba7
Bhagaban Ghadai <102967585+BhagabanGhadai@users.noreply.github.com>
2024-05-13 17:38:20 +0530

    adding private and admin health check api

--- apps/api/healthCheck.js
+++ apps/api/healthCheck.js
@@ -1,7 +1,10 @@
 import express from 'express';
 const healthCheckrouter=express.Router()
-import {healthCheckService} from '../services/healthCheck.js'
+import { verifyJWT,verifyPermission } from '../../middlewares/auth.mw.js';
+import {healthCheckService,healthCheckServiceAdmin,healthCheckServicePrivate} from '../services/healthCheck.js'
 
 healthCheckrouter.route('/health-check').get(healthCheckService)
+healthCheckrouter.route('/health-check/private').get(verifyJWT,healthCheckServicePrivate)
+healthCheckrouter.route('/health-check/admin').get(verifyJWT,verifyPermission,healthCheckServiceAdmin)
 
 export default healthCheckrouter
--- apps/services/healthCheck.js
+++ apps/services/healthCheck.js
@@ -5,3 +5,9 @@ import { catchAsync } from '../../utils/catchAsync.js';
 export const healthCheckService=catchAsync(async(req,res)=>{
     return res.status(200).send(new ApiResponse(200,{},'health check complete and successfull🔋🔋'))
 })
+export const healthCheckServicePrivate=catchAsync(async(req,res)=>{
+    return res.status(200).send(new ApiResponse(200,{},'health check complete and successfull🔋🔋'))
+})
+export const healthCheckServiceAdmin=catchAsync(async(req,res)=>{
+    return res.status(200).send(new ApiResponse(200,{},'health check complete and successfull🔋🔋'))
+})
